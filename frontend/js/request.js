f94ed3f03c838ac647be34230e041a4413aed641
Kevin Simonemai <kevinsimon.pro@gmail.com>
2021-01-13 15:20:28 +0100

    request premier test

--- frontend/js/request.js
+++ frontend/js/request.js
@@ -8,9 +8,12 @@ fetch('http://localhost:3000/api/teddies')
     //response => console.log(response)
 
 
+
+
 //Fetch pour récupérer l'id du produit
+function donnees (response) {
+    console.log(response);
+}
 fetch('http://localhost:3000/api/teddies/5be9c8541c9d440000665243')
     .then (response => response.json())
     .then (ours => console.log(ours))
-
-
